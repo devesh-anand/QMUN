6eecb0abe911e9877e9f5611425f0293d679e1c7
devesh-anand <contactdeveshanand@gmail.com>
2022-11-22 12:18:35 +0530

    analytics added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,23 +3,23 @@ import '@/styles/tailwind.css'
 import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-
-  return(
+  return (
     <>
       <Script
-      strategy='lazyOnload'
-      src='https://www.googletagmanager.com/gtag/js?id=G-NFZ1F82H7Y' />
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-NFZ1F82H7Y"
+      />
 
-      <Script
-      strategy='lazyOnload'
-      src={`
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
         window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'G-NFZ1F82H7Y');`
-      } />
+      gtag('config', 'G-NFZ1F82H7Y');`}
+      </Script>
 
-   <Component {...pageProps} /></>
+      <Component {...pageProps} />
+    </>
   )
 }
