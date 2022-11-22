90d222b31eb10cc0fdf0ce76e0722b6549d0aef9
devesh-anand <contactdeveshanand@gmail.com>
2022-11-22 12:12:04 +0530

    analytics added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,25 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+    <>
+      <Script
+      strategy='lazyOnload'
+      src='https://www.googletagmanager.com/gtag/js?id=G-NFZ1F82H7Y' />
+
+      <Script
+      strategy='lazyOnload'
+      src={`
+        window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-NFZ1F82H7Y');`
+      } />
+
+   <Component {...pageProps} /></>
+  )
 }
